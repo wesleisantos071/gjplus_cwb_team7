5283201817ec5f37d8b8d9c32ad116e93efe1acd
WesleiSantos <west.dunker@gmail.com>
2020-10-10 00:58:47 -0300

    Implemented smooth on movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+    float distance;
+
+    private void Start() {
+        distance = target.position.z - transform.position.z;
+    }
+
+
+    private void LateUpdate() {
+        Vector3 pos = transform.position;
+        if (target.transform.position.z > (pos.z + distance)) {
+            Vector3 targetPos = new Vector3(pos.x, pos.y, target.transform.position.z - distance);
+            transform.position = targetPos;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovementHandler.cs
+++ Assets/Scripts/PlayerMovementHandler.cs
@@ -9,47 +9,70 @@ public class PlayerMovementHandler : MonoBehaviour {
     public float leftX = -0.5f;
     public float middleX = 0;
     public float rightX = 0.5f;
+    public float smoothSpeed = .3f;
+    public float jumpSpeed = 6;
+
+    direction currentDirection = direction.NONE;
+
+    enum direction {
+        NONE,
+        TO_LEFT,
+        RIGHT_TO_MIDDLE,
+        LEFT_TO_MIDDLE,
+        TO_RIGHT
+    }
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
         rb.velocity = Vector3.forward * moveSpeed;
     }
+
     void Update() {
-        float h = HandleInput();
-        Move(h);
+        int h = HandleInput();
+        if (h != 0) {
+            SetDestination(h);
+        }
+        if (currentDirection != direction.NONE) {
+            Move();
+        }
     }
 
-    private void Move(float h) {
+    private void Move() {
+        if (currentDirection == direction.TO_LEFT && transform.position.x > leftX
+            || currentDirection == direction.RIGHT_TO_MIDDLE && transform.position.x > middleX) {
+            Vector3 destinationPos = transform.position;
+            destinationPos.x = destinationPos.x - (jumpSpeed * Time.deltaTime);
+            transform.position = destinationPos;
+        } else if (currentDirection == direction.TO_RIGHT && transform.position.x < rightX
+            || currentDirection == direction.LEFT_TO_MIDDLE && transform.position.x < middleX) {
+            Vector3 destinationPos = transform.position;
+            destinationPos.x = destinationPos.x + (jumpSpeed * Time.deltaTime);
+            transform.position = destinationPos;
+        } else {
+            currentDirection = direction.NONE;
+        }
+        Debug.Log("moving to:" + currentDirection);
+    }
+
+    private void SetDestination(float h) {
         Vector3 currentPos = transform.position;
         if (h > 0) {// move right
             if (currentPos.x < middleX) {
-                currentPos.x = middleX;
-                transform.position = currentPos;
-                return;
+                currentDirection = direction.LEFT_TO_MIDDLE;
+            } else if (currentPos.x < rightX) {
+                currentDirection = direction.TO_RIGHT;
             }
-            if (currentPos.x < rightX) {
-                currentPos.x = rightX;
-                transform.position = currentPos;
-                return;
-            }
-        }
-        if (h < 0) {// move left
+        } else if (h < 0) {// move left
             if (currentPos.x > middleX) {
-                currentPos.x = middleX;
-                transform.position = currentPos;
-                return;
-            }
-            if (currentPos.x > leftX) {
-                currentPos.x = leftX;
-                transform.position = currentPos;
-                return;
+                currentDirection = direction.RIGHT_TO_MIDDLE;
+            } else if (currentPos.x > leftX) {
+                currentDirection = direction.TO_LEFT;
             }
         }
-
     }
 
-    float HandleInput() {
-        float h = 0;
+    int HandleInput() {
+        int h = 0;
         if (Input.GetKeyDown(KeyCode.RightArrow)) {
             h = 1;
         } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
