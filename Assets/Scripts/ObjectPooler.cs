a73d78159041e720708f6837ff3ade0bf0ae6bfe
Weslei <wesleisantos071@gmail.com>
2020-11-29 12:57:47 -0300

    Started implementation of object pool

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    public static ObjectPooler instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    void Start() {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools) {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++) {
+                GameObject go = Instantiate(pool.prefab);
+                go.SetActive(false);
+                objectPool.Enqueue(go);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 pos, Quaternion rotation) {
+        if (poolDictionary.ContainsKey(tag)) {
+            GameObject go = poolDictionary[tag].Dequeue();
+            go.SetActive(true);
+            go.transform.position = pos;
+            go.transform.rotation = rotation;
+            poolDictionary[tag].Enqueue(go);
+            return go;
+        }
+        Debug.LogError("No entry in dictionary for tag:" + tag);
+        return null;
+    }
+}
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour {
     public static PlatformController instance;
-    public GameObject[] emptyPlatformPrefabs;
-    public GameObject[] easyPlatformPrefabs;
-    public float distanceToCreateMorePlatform;
-    GameObject lastPlatform;
+    public GameObject lastPlatform;
     public System.Action<GameObject> onCreatePlaform;
+    public float platformSize;
+    List<string> platformTags;
+    ObjectPooler platformPooler;
 
     private void Awake() {
         if (instance == null) {
@@ -17,53 +17,20 @@ public class PlatformController : MonoBehaviour {
     }
 
     private void Start() {
+        platformPooler = ObjectPooler.instance;
+        platformTags = new List<string>(platformPooler.poolDictionary.Keys);
         lastPlatform = transform.GetChild(0).gameObject;
         CreatePlatform();
     }
 
+
     public void CreatePlatform() {
-        int rand = UnityEngine.Random.Range(0, 2);
-        switch (rand) {
-            case 0:
-                CreatePlatformEmpty();
-                break;
-            case 1:
-                CreatePlatformEasy();
-                break;
-        }
-    }
-
-    private float GetLastZ() {
-        Vector3 lastWorldPoint = new Vector3();
-        foreach (Transform transform in lastPlatform.transform) {
-            if (transform.position.z > lastWorldPoint.z) {
-                lastWorldPoint = transform.position;
-            }
-        }
-        return lastPlatform.transform.InverseTransformVector(lastWorldPoint).z;
-    }
-
-    private void RepositionPlatform(GameObject platform) {
-        Vector3 pos = platform.transform.position;
-        pos.x = lastPlatform.transform.localPosition.x;
-        pos.y = lastPlatform.transform.localPosition.y;
-        pos.z = GetLastZ();
-        platform.transform.localPosition = pos;
-        lastPlatform = platform;
-        onCreatePlaform?.Invoke(lastPlatform);
-    }
-
-    private void CreatePlatformEmpty() {
-        int rand = Random.Range(0, emptyPlatformPrefabs.Length);
-        GameObject prefab = emptyPlatformPrefabs[rand];
-        GameObject go = GameObject.Instantiate(prefab, transform);
-        RepositionPlatform(go);
-    }
-
-    private void CreatePlatformEasy() {
-        int rand = Random.Range(0, easyPlatformPrefabs.Length);
-        GameObject prefab = easyPlatformPrefabs[rand];
-        GameObject go = GameObject.Instantiate(prefab, transform);
-        RepositionPlatform(go);
+        int tagIndex = UnityEngine.Random.Range(0, platformTags.Count);
+        string tagName = platformTags[tagIndex];
+        Vector3 pos = lastPlatform.transform.position;
+        pos.z = pos.z + platformSize;
+        GameObject go = platformPooler.SpawnFromPool(tagName, pos, Quaternion.identity);
+        lastPlatform = go;
+        onCreatePlaform(go);
     }
 }
