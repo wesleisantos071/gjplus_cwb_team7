5756ef3541ba4efa24d5eb80e42cbcbf17b3418e
jessicagoss <jessicamgoss@gmail.com>
2021-01-06 12:46:35 -0300

    Implemented Credits menu

---
+++ Assets/Scripts/Handler/LinkTextHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LinkTextHandler : MonoBehaviour {
+    string url;
+
+    private void Start() {
+        url = GetComponent<TextMeshProUGUI>().text;
+
+        Button button = gameObject.AddComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick() {
+        Application.OpenURL(url);
+    }
+}
